572a107e194830f80909e4dfb99612d1b7a251d9
John Song <jsong@silveroak-labs.com>
2015-02-11 15:45:11 +0800

    add ui framework

---
+++ PlayerClient/src/com/silveroak/playerclient/ui/activity/PlayerSearchActivity.java
@@ -0,0 +1,9 @@
+package com.silveroak.playerclient.ui.activity;
+
+/**
+ * Created by John on 2015/2/10.
+ */
+public class PlayerSearchActivity {
+
+
+}
---
+++ PlayerClient/src/com/silveroak/playerclient/ui/fragment/PlayerTopBarFragment.java
@@ -0,0 +1,7 @@
+package com.silveroak.playerclient.ui.fragment;
+
+/**
+ * Created by John on 2015/2/11.
+ */
+public class PlayerTopBarFragment {
+}
